9007e17ded7692a91eca11d1c1e0cc982d417912
aulichney <annie.ulichney@yale.edu>
2021-04-12 14:50:51 -0400

    Upload

---
+++ dropdown_options.js
@@ -0,0 +1,10 @@
+var dropdown_options = [
+{ value: "numIncarcerated",
+  text: "Number Incarcerated" },
+{ value: "numCrimes",
+  text: "Number of Crimes" },
+{ value: "population",
+  text: "County Population" },
+{ value: "incarcerationRate",
+  text: "Incarceration Rate" },
+]
